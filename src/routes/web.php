52bc6dd58c003d6905b5eaac1e1b6e27e386628a
haruna <news.sigeak@gmail.com>
2025-07-27 08:59:26 +0900

    カテゴリのルーティングを設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
+use App\Http\Controller\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +26,5 @@ Route::post('/todos', [TodoController::class, 'store']);
 Route::patch('/todos/update', [TodoController::class, 'update']);
 
 Route::delete('/todos/delete', [TodoController::class, 'destroy']);
+
+Route::get('/categories', [CategoryController::class, 'index']);
